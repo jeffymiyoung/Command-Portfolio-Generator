b243cd1d40c167b26ff86983479ebd4d0ac27292
Jeffrey Young <jeffreymiyoung@aol.com>
2022-04-25 18:40:34 -0400

    added name input question va inquirer package

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 // Portfolio Generator Application Code
 
 // variable declaration
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// const pageHTML = gemeratePage(Name, GitHub);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'Name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
